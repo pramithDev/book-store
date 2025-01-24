685e6cf1463e2abea11afc818dc2596188746a4c
pulze <malagodap2@gmail.com>
2025-01-24 10:07:00 +0530

    assets and banner changes added.

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -5,7 +5,34 @@ import { HiOutlineUser } from "react-icons/hi";
 import { HiOutlineHeart } from "react-icons/hi";
 import { HiOutlineShoppingCart } from "react-icons/hi2";
 
+import avatarImg from "../assets/avatar.png";
+import { useState } from "react";
+
+const navigation = [
+    {
+        name: 'Dashboard',
+        href: '/dashboard',
+    },
+    {
+        name: 'Orders',
+        href: '/orders',
+    },
+    {
+        name: 'Cart Page',
+        href: '/cart',
+    },
+    {
+        name: 'Check Out',
+        href: '/checkout',
+    },
+]
+
 const Navbar = () => {
+
+    const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+    // console.log(isDropdownOpen);
+
+    const currentUser = true;
     return (
         <header className="max-w-screen-2xl mx-auto px-4 py-6">
             <nav className="flex justify-between items-center">
@@ -25,14 +52,39 @@ const Navbar = () => {
                 </div>
 
                 {/* Right Side */}
-                <div className="flex items-center gap-4">
-                    <HiOutlineUser className="size-6"/>
+                <div className="relative flex items-center md:space-x-3 space-x-2">
+                    <div>
+                        {
+                            currentUser ? <>
+                            <button onClick={() => setIsDropdownOpen(!isDropdownOpen)}>
+                                <img src={avatarImg} alt="User" className={`size-7 rounded-full ${currentUser ? 'ring-2 ring-blue-500' : ''}`} />
+                            </button>
+                            {/* show dropdown */}
+                            {
+                                isDropdownOpen && (
+                                    <div className="absolute right-0 mt-2 w-48 bg-white shadow-lg rounded-md z-40">
+                                        <ul className="py-2">
+                                            {
+                                                navigation.map((item, index) => (
+                                                    <li key={index} onClick={() => setIsDropdownOpen(false)}>
+                                                        <Link to={item.href} className="block py-2 px-4 text-sm hover:bg-gray-200">{item.name}</Link>
+                                                    </li>
+                                                ))
+                                            }
+                                        </ul>
+                                    </div>
+                                )
+                            }
+                            </> : <Link to="/login"><HiOutlineUser className="size-6"/></Link>
+                        }
+                    </div>
+                    
                     <button className="hidden sm:block">
                         <HiOutlineHeart className="size-6"/>
                     </button>
                     <Link to="/cart" className="bg-primary p-1 sm:px-6 px-2 flex items-center rounded-sm">
                         <HiOutlineShoppingCart className="size-6" />
-                        <span>0</span>
+                        <span className="text-sm font-semibold sm:ml-1">0</span>
                     </Link>
                 </div>
             </nav>
---
+++ frontend/src/pages/home/Banner.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Banner = () => {
+  return (
+    <div>
+        <div>
+            <h1 className='md:text-5xl text-2xl font-medium mb-7'>New Releases This Week</h1>
+            <p>
+                It's time to update your reading list with some of the latest and greatest releases in the 
+                literary world. From heart-pumping thrillers to captivating memoirs, this week's new releases 
+                offer something for everyone
+            </p>
+        </div>
+        <div>
+
+        </div>
+    </div>
+  )
+}
+
+export default Banner
