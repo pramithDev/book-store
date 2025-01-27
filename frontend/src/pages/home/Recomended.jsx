442b31fc2bc2a5ae9b5e1c2deed972755d02b6ca
pulze <malagodap2@gmail.com>
2025-01-27 21:25:51 +0530

    login and register added.

--- frontend/src/pages/home/Recomended.jsx
+++ frontend/src/pages/home/Recomended.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react'
 import BookCard from '../books/BookCard';
 
 // Import Swiper React components
@@ -12,11 +12,8 @@ import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 
-const categories = ["Choose a genre", "Business", "Fiction", "Horror", "Adventure"];
-
-const TopSellers = () => {
+const Recomended = () => {
     const [books, setBooks] = useState([]);
-    const [selectedCategory, setSelectedCategory] = useState("Choose a genre");
 
     useEffect(() => {
         fetch("books.json")
@@ -24,32 +21,14 @@ const TopSellers = () => {
             .then((data) => setBooks(data))
     }, []);
 
-    const filteredBooks = selectedCategory === "Choose a genre" ? books : books.filter(book => book.category === selectedCategory.toLowerCase());
-
-    console.log(filteredBooks);
-
     return (
-        <div className='py-10'>
-            <h2 className='text-3xl font-semibold mb-6'>Top Sellers</h2>
-            {/* { Category Filtering } */}
-            <div className='mb-8 flex items-center'>
-                <select
-                    onChange={(e) => setSelectedCategory(e.target.value)}
-                    name="category" id="category" className='border bg-[#EAEAEA] border-gray-300 rounded-md px-4 py-2 focus:outline-none'>
-                    {
-                        categories.map((category, index) => (
-                            <option key={index} value={category}>{category}</option>
-                        ))
-                    }
-                </select>
-            </div>
+        <div className='py-16'>
+            <h2 className='text-3xl font-semibold mb-6'>Recommended for you</h2>
 
             <Swiper
                 navigation={true}
                 slidesPerView={1}
                 spaceBetween={30}
-                observer={false}
-                observeParents={true}
                 breakpoints={{
                     640: {
                         slidesPerView: 1,
@@ -66,9 +45,6 @@ const TopSellers = () => {
                     1180: {
                         slidesPerView: 3,
                         spaceBetween: 50,
-                        // navigation: {
-                        //     disabledClass: 'uh',
-                        // }
                     },
                 }}
                 modules={[Pagination, Navigation]}
@@ -76,7 +52,7 @@ const TopSellers = () => {
             >
 
                 {
-                    filteredBooks.length > 0 && filteredBooks.map((book, index) => (
+                    books.length > 0 && books.slice(8, 18).map((book, index) => (
                         <SwiperSlide key={index}>
                             <BookCard book={book} />
                         </SwiperSlide>
@@ -87,4 +63,4 @@ const TopSellers = () => {
     )
 }
 
-export default TopSellers
+export default Recomended
